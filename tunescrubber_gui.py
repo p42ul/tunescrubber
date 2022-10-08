b23679f449832f649879e32af24f5fc9b874fd19
Paul Buser <buser.paul@gmail.com>
2022-10-08 18:51:13 -0400

    Reorganize imports. Add support for stereo and mono files.

--- tunescrubber_gui.py
+++ tunescrubber_gui.py
@@ -6,21 +6,24 @@
 from time import sleep
 import logging
 import threading
+import wave
 
 # Third-party libraries
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.pyplot import figure, show 
 from scipy.io import wavfile
+from scipy.signal import hilbert
 from scipy.signal import hilbert
 from sklearn import preprocessing
 import PySimpleGUI as sg
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
+import simpleaudio as sa
 import serial
 import serial.tools.list_ports
-from scipy.signal import hilbert
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.pyplot import figure, show 
-import matplotlib.pyplot as plt
-import matplotlib
+
 matplotlib.use('TkAgg')
 
 GRAPH_SIZE = (400,300)
@@ -53,7 +56,7 @@ def window_function():
         [sg.Button('Quit')],
     ]
     # Create the Window
-    window = sg.Window('Serial Communicator', [layout], resizable=True, finalize=True)
+    window = sg.Window('TuneScrubber', [layout], resizable=True, finalize=True)
 
     plt.figure(1)
     plt.subplot(111)
@@ -73,8 +76,13 @@ def window_function():
                 ser.open()
             open_port.update(f'Current port: {port}')
         elif event == 'File':
-            sample_rate, audio_buffer = wavfile.read(values['File'])
+            filepath = values['File']
+            with wave.Wave_read(filepath) as w:
+                bytes_per_sample = w.getsampwidth()
+                num_channels = w.getnchannels()
+            sample_rate, audio_buffer = wavfile.read(filepath)
             envelope = calc_envelope(audio_buffer)
+            sa.play_buffer(audio_buffer, num_channels=num_channels, bytes_per_sample=bytes_per_sample, sample_rate=sample_rate)
             plt.clf()
             envPlot.plot(envelope)
             fig_canvas_agg = draw_figure(fig_canvas_agg)
@@ -85,100 +93,21 @@ def window_function():
             torque = values['Zoom']
             if ser.is_open:
                 ser.write(bytes(str(int(torque)), 'utf-8'))
-        elif event == 'Reset':
-            torque_slider.update(0)
-            torque = 0
-            if ser.is_open:
-                ser.write(bytes(str(0), 'utf-8'))
             
     window.close()
 
 # Calculates the amplitude envelope of the audio file
 def calc_envelope(buffer):
-    analytic_signal = np.abs(hilbert(buffer)[:, 0])
+    if type(buffer[0]) == list:
+        analytic_signal = np.abs(hilbert(buffer)[:, 0])
+    else:
+        analytic_signal = np.abs(hilbert(buffer))
     return analytic_signal / np.linalg.norm(analytic_signal)
 
 def draw_figure(figure_canvas_agg):
     figure_canvas_agg.draw()
     figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
     return figure_canvas_agg
-
-class ZoomPan:
-    def __init__(self):
-        self.cur_xlim = None
-        self.cur_ylim = None
-        self.x0 = None
-        self.y0 = None
-        self.x1 = None
-        self.y1 = None
-
-    def zoom_factory(self, ax, base_scale = 2.):
-        def zoom(event):
-            cur_xlim = ax.get_xlim()
-            cur_ylim = ax.get_ylim()
-
-            xdata = event.xdata # get event x location
-            ydata = event.ydata # get event y location
-
-            if event.button == 'up':
-                # deal with zoom in
-                scale_factor = 1 / base_scale
-            elif event.button == 'down':
-                # deal with zoom out
-                scale_factor = base_scale
-            else:
-                # deal with something that should never happen
-                scale_factor = 1
-                print (event.button)
-
-            new_width = (cur_xlim[1] - cur_xlim[0]) * scale_factor
-            new_height = (cur_ylim[1] - cur_ylim[0]) * scale_factor
-
-            relx = (cur_xlim[1] - xdata)/(cur_xlim[1] - cur_xlim[0])
-            rely = (cur_ylim[1] - ydata)/(cur_ylim[1] - cur_ylim[0])
-
-            ax.set_xlim([xdata - new_width * (1-relx), xdata + new_width * (relx)])
-            ax.set_ylim([ydata - new_height * (1-rely), ydata + new_height * (rely)])
-            ax.figure.canvas.draw()
-
-        fig = ax.get_figure() # get the figure of interest
-        fig.canvas.mpl_connect('scroll_event', zoom)
-
-        return zoom
-
-    def pan_factory(self, ax):
-        def onPress(event):
-            if event.inaxes != ax: return
-            self.cur_xlim = ax.get_xlim()
-            self.cur_ylim = ax.get_ylim()
-            self.press = self.x0, self.y0, event.xdata, event.ydata
-            self.x0, self.y0, self.xpress, self.ypress = self.press
-
-        def onRelease(event):
-            self.press = None
-            ax.figure.canvas.draw()
-
-        def onMotion(event):
-            if self.press is None: return
-            if event.inaxes != ax: return
-            dx = event.xdata - self.xpress
-            dy = event.ydata - self.ypress
-            self.cur_xlim -= dx
-            self.cur_ylim -= dy
-            ax.set_xlim(self.cur_xlim)
-            ax.set_ylim(self.cur_ylim)
-
-            ax.figure.canvas.draw()
-
-        fig = ax.get_figure() # get the figure of interest
-
-        # attach the call back
-        fig.canvas.mpl_connect('button_press_event',onPress)
-        fig.canvas.mpl_connect('button_release_event',onRelease)
-        fig.canvas.mpl_connect('motion_notify_event',onMotion)
-
-        #return the function
-        return onMotion
 
 def serial_read_thread():
     absolute_position = None
@@ -196,15 +125,18 @@ def serial_read_thread():
         if absolute_position is None:
             absolute_position = angle
             last_angle = angle
-        else:
-            # Angle goes from Quadrant 4 to Quadrant 1
-            if angle < ANGLE_MAX // 4 and last_angle > (ANGLE_MAX // 4)*3:
-                last_angle -= ANGLE_MAX
-            # Angle goes from Quadrant 1 to Quadrant 4
-            if angle > (ANGLE_MAX // 4)*3 and last_angle < ANGLE_MAX // 4:
-                last_angle += ANGLE_MAX
-            absolute_position += (last_angle - angle)
-            last_angle = angle
+            continue
+        # Angle goes from Quadrant 4 to Quadrant 1
+        if angle < ANGLE_MAX // 4 and last_angle > (ANGLE_MAX // 4)*3:
+            last_angle -= ANGLE_MAX
+        # Angle goes from Quadrant 1 to Quadrant 4
+        if angle > (ANGLE_MAX // 4)*3 and last_angle < ANGLE_MAX // 4:
+            last_angle += ANGLE_MAX
+        delta = last_angle - angle
+        inverse_torque = delta / 10
+        ser.write(bytes(str(inverse_torque), 'utf-8'))
+        absolute_position += delta
+        last_angle = angle
         print(absolute_position)
 
 
