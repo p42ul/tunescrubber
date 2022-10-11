af5f8d1d8f6cb1ca52632873aee5c76546f83092
Paul Buser <buser.paul@gmail.com>
2022-10-10 21:16:10 -0400

    Everything is working! Got rid of matplotlib

--- tunescrubber_gui.py
+++ tunescrubber_gui.py
@@ -11,21 +11,14 @@ import threading
 import wave
 
 # Third-party libraries
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.pyplot import figure, show 
 from scipy.io import wavfile
 from scipy.signal import hilbert, tukey
 from sklearn import preprocessing
 import PySimpleGUI as sg
-import matplotlib
-import matplotlib.pyplot as plt
-import matplotlib.pyplot as plt
 import numpy as np
 import simpleaudio as sa
 import serial
 import serial.tools.list_ports
-
-matplotlib.use('TkAgg')
 
 GRAPH_SIZE = (400,300)
 ANGLE_MAX = 3600
@@ -37,6 +30,7 @@ ser.baudrate = 115200
 playback_buffer = deque()
 sample_rate = audio_buffer = num_channels = bytes_per_sample = None
 playhead_position = 0
+playhead_line = None
 seconds_per_rotation = 1
 
 torque_multiplier = 5000
@@ -47,11 +41,12 @@ envelope = None
 
 ports_dropdown = sg.Combo((), readonly=True, size=(20, 5), key='Ports')
 zoom_slider = sg.Slider(range=(1, 3), default_value=seconds_per_rotation, enable_events=True, orientation='horizontal', key='Zoom')
-canvas = sg.Canvas(key='Canvas')
+graph = sg.Graph(canvas_size=GRAPH_SIZE, graph_bottom_left=(0,0), graph_top_right=GRAPH_SIZE, key='Graph')
 open_port = sg.Text('')
 file_browse = sg.FileBrowse(button_text='Open File', key='File', file_types = (('.wav files', '*.wav'),), enable_events=True) 
 position_indicator = sg.ProgressBar(max_value=3600, orientation='horizontal', key='position_indicator')
 torque_slider = sg.Slider(range=(0, 25000), default_value=torque_multiplier, key='TorqueSlider', enable_events=True, orientation='horizontal')
+
 
 sg.change_look_and_feel('LightGreen')
 def window_function():
@@ -65,33 +60,28 @@ def window_function():
     # All the stuff inside your window.
     layout = [
         [sg.Text('Ports'), ports_dropdown, sg.Button('Refresh')],
-        [sg.Button('Open Port'), open_port],
+        [sg.Button('Open Port', key='OpenButton'), open_port],
         [file_browse],
-        [canvas],
+        [graph],
         [position_indicator],
         # [sg.Text('Zoom'), zoom_slider, sg.Button('Reset')],
-        [torque_slider],
+        [sg.Text('Sensitivity'), torque_slider],
         [sg.Button('Clear')],
         [sg.Button('Quit')],
     ]
     # Create the Window
     window = sg.Window('TuneScrubber', [layout], resizable=True, finalize=True)
-
-
-    plt.figure(1)
-    plt.subplot(111)
-    fig = matplotlib.figure.Figure(figsize=(5, 4), dpi=100)
-    envPlot = fig.add_subplot(111)
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Quit': # if user closes window or clicks cancel
             break
-        elif event == 'Open Port':
+        elif event == 'OpenButton':
             port = values['Ports']
             ser.port = port
             if not ser.is_open:
                 ser.open()
             open_port.update(f'Current port: {port}')
+            window['OpenButton'].update(disabled=True)
         elif event == 'File':
             filepath = values['File']
             with wave.Wave_read(filepath) as w:
@@ -101,10 +91,9 @@ def window_function():
             if num_channels > 1:
                 audio_buffer = audio_buffer[:, 0] # quick hack to get mono signal
                 num_channels = 1
+            graph.erase()
             envelope = calc_envelope(audio_buffer)
-            fig_canvas_agg = FigureCanvasTkAgg(fig, canvas.TKCanvas)
-            envPlot.plot(envelope)
-            fig_canvas_agg = draw_figure(fig_canvas_agg)
+            draw_envelope()
         elif event == 'Refresh':
             ports = serial.tools.list_ports.comports()
             ports_dropdown.update(values=[port for (port, desc, hwid) in ports])
@@ -112,14 +101,32 @@ def window_function():
             torque_multiplier = values['TorqueSlider']
         elif event == 'Clear':
             print('clearing canvas')
-            fig_canvas_agg.get_tk_widget().forget()
-            plt.cla()
-            plt.clf()
-            fig_canvas_agg = draw_figure(fig_canvas_agg)
-
+            envelope = None
+            graph.erase()
 
             
     window.close()
+
+def draw_envelope():
+    w, h = GRAPH_SIZE
+    # Draw envelope
+    if envelope is not None:
+        samples_per_pixel = len(envelope) // w
+        for x in range(w-1):
+            val = envelope[x * samples_per_pixel] * h * 200 # dirty hack to "normalize"
+            graph.draw_line((x, 0), (x, val), color='white')
+
+def draw_playhead():
+    global playhead_line
+    if envelope is None:
+        return
+    w, h = GRAPH_SIZE
+    samples_per_pixel = len(envelope) // w
+    if playhead_line is not None:
+        graph.delete_figure(playhead_line)
+    # Draw playhead
+    x = playhead_position // samples_per_pixel
+    playhead_line = graph.draw_line((x, 0), (x, h), color='red')
 
 
 # Calculates the amplitude envelope of the audio file
@@ -129,11 +136,6 @@ def calc_envelope(buffer):
     else:
         analytic_signal = np.abs(hilbert(buffer))
     return analytic_signal / np.linalg.norm(analytic_signal)
-
-def draw_figure(figure_canvas_agg):
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-    return figure_canvas_agg
 
 def serial_read_thread():
     global playhead_position, playback_buffer
@@ -185,6 +187,7 @@ def serial_read_thread():
         chunk = np.multiply(chunk, window).astype(target_type)
         playback_buffer.appendleft(chunk)
         playhead_position = new_playhead_position
+        draw_playhead()
 
 def playback_thread():
     global playback_buffer
